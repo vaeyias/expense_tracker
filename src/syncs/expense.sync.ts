1da67754fa8182fcf4d54c3cd36370ad45b112e6
Vy Pham <thuvytpham@gmail.com>
2025-11-07 18:14:53 -0500

    :construction: fix update debt

--- src/syncs/expense.sync.ts
+++ src/syncs/expense.sync.ts
@@ -75,7 +75,7 @@ export const AddUserSplitResponse: Sync = (
   ),
   then: user !== payer
     ? actions(
-      [Debt.updateDebt, { payer, user, amountOwed }],
+      [Debt.updateDebt, { payer, receiver: user, amountOwed }],
       [Requesting.respond, { request }],
     )
     : actions(
